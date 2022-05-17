573afa5f4f0ebd1270c38e91ea54e91d6f5e7524
Maheshkumar P <m.palanisamy@saama.com>
2022-05-17 15:04:08 +0530

    Create update blog view

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,4 +1,5 @@
-from turtle import pos
+from pydoc import getpager
+from turtle import pos, title
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -44,6 +45,34 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
 
 
 def get_post(id, check_author=True):
